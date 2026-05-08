4474ca3726c9004d9c2d6de0eb2b3a5d2d6c5dbc
KennyBabooga <jwillows777@gmail.com>
2026-05-08 10:01:18 -0400

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
         <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          Fundamental React concepts you will need for almost any app you are
+          {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
@@ -14,6 +14,48 @@ function App() {
       </main>
     </div>
   );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept 
+              title = "Components"
+              description = "The Core UI Building Block."
+              image={componentsImg}
+            />
+            <CoreConcept 
+              title = {CORE_CONCEPTS[0].title}
+              description = {CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept />
+          </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
+const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
+
+function genRandomInt(max){
+  return Math.floor(Math.random() * (max + 1));
 }
 
 export default App;
